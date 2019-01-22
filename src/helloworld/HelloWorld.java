61ddc8e883d9e0c8c9adf69eb9e684122e8de327
Johnny <Johnny@10.10.152.60>
2019-01-22 12:57:32 -0600

    Test net beans

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+//Green, John G.
+//CIT295 Software Dvpt Process
+//Hello World
+//Program Displays, "Hello World."
+package helloworld;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
+}
